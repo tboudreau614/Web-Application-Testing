60420a1fe286f7d9758d11a0dbf79460b12a4f97
Tristan Boudreau <tristanboudreau@Tristans-MacBook-Pro.local>
2019-10-31 16:42:18 -0600

    update

--- web-app-testing/src/App.test.js
+++ web-app-testing/src/App.test.js
@@ -4,13 +4,10 @@ import '@testing-library/jest-dom/extend-expect'
 import App from './App';
 import Dashboard from './components/Dashboard'
 
-
-
 test('renders without crashing', () => {
   const wrapper = rtl.render(<App/>)
   expect(wrapper).toBeVisible
 });
-
 
 test('renders dashboard component', () => {
   const wrapper = rtl.render(<Dashboard/>)
